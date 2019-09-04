2b1fbc57b58b655fc3080aca788feba2b64a1d49
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2019-09-04 12:19:52 +0200

    comments

--- utility/utility.go
+++ utility/utility.go
@@ -12,6 +12,7 @@ import (
 //URL è il link all'offerta lampo per kindle store
 const URL = "https://www.amazon.it/Offerta-Lampo-Kindle/b?ie=UTF8&node=5689487031"
 
+//Deal è la struttura del libro in offerta lampo
 type Deal struct {
 	Title  string
 	Author string
